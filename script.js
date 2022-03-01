67dcbc21b374036f8572fd2c23bc17bdc10842a3
hrosenskjold <47818937+hrosenskjold@users.noreply.github.com>
2022-03-01 12:55:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ let config = {
   maxZoom: 18,
 };
 // magnification with which the map will start
-const zoom = 18;
+const zoom = 12;
 // co-ordinates
 
-const lat = 52.22977;
-const lng = 21.01178;
+const lat = 56.22977;
+const lng = 9.01178;
 
 // calling map
 const map = L.map("map", config).setView([lat, lng], zoom);
